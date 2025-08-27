42e0ee322a110543a01415ffab5d0139c928f026
Afaq-Ahmad-eng <afaqahmad2593@gmail.com>
2025-08-27 15:11:56 +0500

    We created a new LandingSection.jsx file and copied the code from the existing LandingSection.js file. The previous Portfolio was built with CRA, but since CRA is deprecated, we are now rebuilding the Portfolio using Vite.

---
+++ src/components/landingSection/LandingSection.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Avatar, Heading, VStack } from "@chakra-ui/react";
+import FullScreenSection from "./FullScreenSection";
+import picture from "../images/IMG_0210.jpg"
+const greeting = "Hello, I am Afaq Ahmad!";
+const bio1 = "A frontend developer";
+const bio2 = "specialised in React";
+
+// Implement the UI for the LandingSection component according to the instructions.
+// Use a combination of Avatar, Heading and VStack components.
+const LandingSection = () => (
+  <FullScreenSection
+    justifyContent="center"
+    alignItems="center"
+    isDarkBackground
+    backgroundColor="#2A4365"
+  >
+   <Avatar name="Afaq Ahmad" src={picture} size="xl"/>
+  <VStack>
+  <Heading>{bio1}</Heading>
+  <Heading>{bio2}</Heading>
+  </VStack>
+  </FullScreenSection>
+);
+
+export default LandingSection;
