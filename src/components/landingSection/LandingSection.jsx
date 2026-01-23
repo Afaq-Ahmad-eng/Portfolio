7f6d5d574607a1a4d85280ecb7924d3a721cd62a
Afaq-Ahmad-eng <afaqahmad2593@gmail.com>
2026-01-23 23:57:21 +0500

    We set the landing section like we set the image and bio horizentally.

--- src/components/landingSection/LandingSection.jsx
+++ src/components/landingSection/LandingSection.jsx
@@ -1,9 +1,14 @@
-import { Avatar, Heading, VStack } from "@chakra-ui/react";
+import { Image, Heading, VStack,HStack } from "@chakra-ui/react";
 import FullScreenSection from "../fullScreenSection/FullScreenSection";
 import picture from "../../assets/images/IMG_0210.jpg"
 const greeting = "Hello, I am Afaq Ahmad!";
-const bio1 = "A frontend developer";
-const bio2 = "specialised in React";
+const bio1 = "Hi, I’m Afaq Ahmad - Software Developer";
+const bio2 = [
+  "Proficient in React",
+  "Experienced with Backend Technologies",
+  "Learning Software Testing",
+  "Exploring IT Automation with Python"
+];
 
 // Implement the UI for the LandingSection component according to the instructions.
 // Use a combination of Avatar, Heading and VStack components.
@@ -14,11 +19,26 @@ const LandingSection = () => (
     isDarkBackground
     backgroundColor="#2A4365"
   >
-   <Avatar name="Afaq Ahmad" src={picture} size="xl"/>
+    <HStack spacing={16}>
   <VStack>
   <Heading>{bio1}</Heading>
-  <Heading>{bio2}</Heading>
+  <VStack spacing={2}>
+  {bio2.map((line,index) => (
+    <Heading 
+    key={index}
+    size="l"
+    >{line}</Heading>
+  ))}
   </VStack>
+  </VStack>
+  <Image 
+  src={picture} 
+  alt="Ali"
+  boxSize="300px"
+  objectFit="cover"
+  borderRadius="lg"
+  />
+  </HStack>
   </FullScreenSection>
 );
 
