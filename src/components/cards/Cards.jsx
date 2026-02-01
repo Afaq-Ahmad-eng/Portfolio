0b223f9ea673449e123144f464ebd09c2906a5f8
Afaq-Ahmad-eng <afaqahmad2593@gmail.com>
2026-02-01 23:26:10 +0500

    we adjust this. we set card as a link when some click on individual card then that card will redirect that user to the website or to the github repo in the case of if the project is not deployed.

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,28 +1,56 @@
-import {Heading, Card, CardBody,HStack, Image, Text, VStack } from "@chakra-ui/react";
+import {
+  Heading,
+  Card,
+  CardBody,
+  HStack,
+  Image,
+  Text,
+  VStack,
+  Link as ChakraLink,
+} from "@chakra-ui/react";
+import { Link as RouterLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
+const Cards = ({ title, description, imageSrc, link }) => {
+  const isExternal = typeof link === "string" && /^(https?:)?\/\//i.test(link);
 
-const Cards = ({ title, description, imageSrc }) => {
-  // Implement the UI for the Card component according to the instructions.
-  // You should be able to implement the component with the elements imported above.
-  // Feel free to import other UI components from Chakra UI if you wish to.
-  return (
-    
-    <Card borderRadius="lg">
-    <Image src={imageSrc} borderRadius="lg"/>
-    <CardBody>
-    <VStack align="start" bg="white" px="sm">
-    <Heading as="h3" size="md">{title}</Heading>
-    <Text color={"gray"} fontSize="sm">{description}</Text>
-    <HStack>
-    <Text>See more</Text>
-    <FontAwesomeIcon icon={faArrowRight} size="1x"/>
-    </HStack>
-    </VStack>
-    </CardBody>
+  const card = (
+    <Card
+      cursor={"pointer"}
+      borderRadius="lg"
+      _hover={{ boxShadow: "lg", transform: "translateY(-4px)" }}
+      transition="0.2s"
+    >
+      <Image src={imageSrc} borderRadius="lg" />
+      <CardBody>
+        <VStack align="start" bg="white">
+          <Heading as="h3" size="md">
+            {title}
+          </Heading>
+          <Text color="gray.600" fontSize="l">
+            {description}
+          </Text>
+          <HStack>
+            <Text>See more</Text>
+            <FontAwesomeIcon icon={faArrowRight} size="1x" />
+          </HStack>
+        </VStack>
+      </CardBody>
     </Card>
-  )
+  );
+
+  if (!link) return card;
+
+  return isExternal ? (
+    <ChakraLink href={link} isExternal _hover={{ textDecoration: "none" }}>
+      {card}
+    </ChakraLink>
+  ) : (
+    <ChakraLink as={RouterLink} to={link} _hover={{ textDecoration: "none" }}>
+      {card}
+    </ChakraLink>
+  );
 };
 
 export default Cards;
